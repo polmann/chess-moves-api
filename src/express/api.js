0f403410af94901180afd326f33f09d02262f388
Eduardo Polmann <epolmann.e@interactiveservices.com>
2019-07-03 16:11:55 -0300

    Add cors

--- src/express/api.js
+++ src/express/api.js
@@ -2,6 +2,7 @@ import express from 'express';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import session from 'express-session';
+import cors from './cors';
 import errorHandler from './errorHandler';
 import healthCheck from './healthCheck';
 import requestLogger from './requestLogger';
@@ -12,6 +13,8 @@ export default function api({ cookieSecret, cookieDomain }) {
   server.enable('trust proxy');
 
   server.use(helmet());
+
+  server.use(cors());
   server.use(requestLogger());
   server.use(bodyParser.json({ limit: '10mb' }));
   server.use(
